7f1d006f4ffbf7b3da485a8d69cb13f789654366
Brian Orsua <brianorsua@gmail.com>
2022-09-29 00:43:44 -0700

    Initialize eventListener for all grid squares

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function createGrid() {
 }
 
 createGrid();
+
+let squares = document.querySelectorAll('.grid');
+squares.forEach(grid => grid.addEventListener('hover', () => {
+  //when mouse hovers over square -> change color
+    //change border-color
+      //can either change via script or by adding active class
+        //create condition/timer for class/color removal
+
+}))
