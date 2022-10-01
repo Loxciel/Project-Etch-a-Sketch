e472eecbb54713ffc824380024e7c1014b94d634
Brian Orsua <brianorsua@gmail.com>
2022-09-30 21:12:56 -0700

    Add eventlistener function to add .filled to selected div

--- script.js
+++ script.js
@@ -9,11 +9,21 @@ function createGrid() {
 
 createGrid();
 
+// function fill(e) {
+//   console.log(e);
+//   //select grid piece 
+//   e.classList.add('filled');
+//     //give filled class
+
+// }
+
 let squares = document.querySelectorAll('.grid');
-squares.forEach(grid => grid.addEventListener('hover', () => {
+squares.forEach(grid => grid.addEventListener('mouseover', (event) => {
+  event.target.classList.add('filled');
+}
   //when mouse hovers over square -> change color
+
     //change border-color
       //can either change via script or by adding active class
         //create condition/timer for class/color removal
-
-}))
+))
