e4ee91f0a62072733119a8de09d0af7b985b84a1
Brian Orsua <brianorsua@gmail.com>
2022-10-02 23:59:24 -0700

    Initalize addSquare()

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-function createGrid() {
-  for(let i = 0; i < 16; i++) {
+function createGrid(num = 16) {
+  for(let i = 0; i < num; i++) {
     let square = document.createElement('div');
     square.classList.add('grid');
     document.getElementById('container').appendChild(square);
@@ -9,21 +9,23 @@ function createGrid() {
 
 createGrid();
 
-// function fill(e) {
-//   console.log(e);
-//   //select grid piece 
-//   e.classList.add('filled');
-//     //give filled class
-
-// }
 
 let squares = document.querySelectorAll('.grid');
 squares.forEach(grid => grid.addEventListener('mouseover', (event) => {
   event.target.classList.add('filled');
 }
   //when mouse hovers over square -> change color
-
-    //change border-color
-      //can either change via script or by adding active class
-        //create condition/timer for class/color removal
 ))
+
+//btn on top of screen that asks user to input amt of squares on each end
+  //new grid should be same size
+    //grid-squares should auto fill space equally
+  //max number of input should be 100
+  function addSquare() {
+    let num = prompt('Add Squares', '1');
+    if (num.isInteger() == true && num <= 100) {
+      //take num.value and append to both sides of grid
+        //input into createGrid
+        console.log(num);
+    }
+  }
